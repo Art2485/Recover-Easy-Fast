a6da07b5ee45d19ec98ea0e9f08be28be2c2daaf
Art2485 <ooartyguitaroo@gmail.com>
2025-08-12 10:25:05 +0700

    Update MainActivity.kt

--- app/src/main/java/com/recovereasy/MainActivity.kt
+++ app/src/main/java/com/recovereasy/MainActivity.kt
@@ -2,27 +2,10 @@ package com.recovereasy
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val rv = findViewById<RecyclerView>(R.id.recycler)
-        rv.layoutManager = LinearLayoutManager(this)
-        val adapter = MediaAdapter()
-        rv.adapter = adapter
-
-        adapter.submitList(
-            List(20) { i ->
-                MediaItem(
-                    title = "Item #$i",
-                    subtitle = "Subtitle $i",
-                    imageUrl = "https://picsum.photos/seed/$i/200/200"
-                )
-            }
-        )
     }
 }
